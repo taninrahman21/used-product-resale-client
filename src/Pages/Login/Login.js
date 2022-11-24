31967ba09a77db854ff7f39216c3b07b07df3ed3
Tanin Rahman <taninr87@gmail.com>
2022-11-24 12:03:11 +0600

    complete signup and login without function just style

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -22,14 +22,16 @@ const Login = () => {
       </div>
      <form onSubmit={handleSubmit(handleLogin)} className='mt-4'>          
           <label className='label'>Email Address</label>
-          <input className='border p-3 mb-3 w-full' {...register("email")} />
+          <input className='border p-3 mb-3 w-full' {...register("email")} placeholder='Email Address'/>
           <label className='label'>Password</label>
-          <input className='border p-3 w-full' {...register("password")} />
+          <input className='border p-3 w-full' {...register("password")} placeholder='Password'/>
           <label className="label text-[#fd8f5f]">Forgot Password?</label>
           <div className='flex justify-between items-center'>
             <p className='mt-3'>New to BecheDaw.com? <Link className='text-[#fd8f5f]' to='/signup'>Create an account.</Link></p>
             <input className='border px-8 py-2 mt-5 bg-[#fd8f5f] text-white' type="submit" value='Login' />
           </div>
+          <div className="divider">OR</div>
+          <button className='border px-8 text-[#fd8f5f] py-2 w-full hover:bg-[#fd8f5f] hover:text-white border-[#fd8f5f]'><Link>Continue With Google</Link></button>
     </form>
     </div>
    </div>
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import img from '../../assets/banner.png';
 
 const Signup = () => {
@@ -15,19 +16,27 @@ const Signup = () => {
    }}>
      <div className='w-2/5 my-16 mx-auto p-10 bg-white'>
       <div className='text-center'>
-        <h2 className='text-3xl font-bold'>Login</h2>
-        <p className='text-sm text-gray-500 mt-3'>Enter Login details to get access</p>
+        <h2 className='text-3xl font-bold'>Sign Up</h2>
+        <p className='text-sm text-gray-500 mt-3'>Enter SignUp details to get access</p>
       </div>
      <form onSubmit={handleSubmit(handleSignup)} className='mt-4'>          
+          <label className='label'>Full Name</label>
+          <input className='border p-3 mb-3 w-full' {...register("name")} placeholder='Enter Full Name'/>
           <label className='label'>Email Address</label>
-          <input className='border p-3 mb-3 w-full' {...register("email")} />
+          <input className='border p-3 mb-3 w-full' {...register("email")} placeholder='Enter Email Address'/>
+          <label className='label'>Who you are?</label>
+          <select {...register('role')} className="select select-bordered w-full">
+            <option defaultValue>Buyer</option>
+            <option>Seller</option>
+          </select>
           <label className='label'>Password</label>
-          <input className='border p-3 w-full' {...register("password")} />
-          <label className="label text-[#fd8f5f]">Forgot Password?</label>
+          <input type='password' className='border p-3 w-full' {...register("password")} placeholder='Enter Password'/>
           <div className='flex justify-between items-center'>
-            <p className='mt-3'>New to BecheDaw.com? <Link className='text-[#fd8f5f]' to='/signup'>Create an account.</Link></p>
-            <input className='border px-8 py-2 mt-5 bg-[#fd8f5f] text-white' type="submit" value='Login' />
+            <p className='mt-3'>Already have an account? <Link className='text-[#fd8f5f]' to='/login'>Login.</Link></p>
+            <input className='border px-8 py-2 mt-5 bg-[#fd8f5f] text-white' type="submit" value='Sign Up' />
           </div>
+          <div className="divider">OR</div>
+          <button className='border text-[#fd8f5f] px-8 py-2 w-full hover:bg-[#fd8f5f] hover:text-white border-[#fd8f5f]'><Link>Continue With Google</Link></button>
     </form>
     </div>
    </div>
